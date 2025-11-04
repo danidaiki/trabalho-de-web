7be77c654d0ba6a525f2f5a3f754f9cd54899b06
Miguel Ap. <miguelaparecidocosta84@gmail.com>
2025-11-04 14:11:09 -0300

    Edit vendedores.js

---
+++ Vendas de Carros/routes/vendedores.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/vendedorController');
+router.get('/', ctrl.index);
+router.get('/new', ctrl.newForm);
+router.post('/', ctrl.create);
+router.get('/:id/edit', ctrl.editForm);
+router.post('/:id', ctrl.update);
+router.post('/:id/delete', ctrl.remove);
+module.exports = router;
