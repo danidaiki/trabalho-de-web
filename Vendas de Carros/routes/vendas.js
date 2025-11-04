8a914d04ee3a8de1a6bed677d8ea886ae83650e1
Gustavo123 <gustavobueno112009@gmail.com>
2025-11-04 13:18:05 -0300

    Delete Vendas de Carros directory

--- Vendas de Carros/routes/vendas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/vendaController');
-
-router.get('/', ctrl.index);
-router.post('/', ctrl.create);
-router.post('/:id/delete', ctrl.remove);
-
-module.exports = router;
