ca9c468825f94e8561f535e9ff557a84846f22d5
Gustavo123 <gustavobueno112009@gmail.com>
2025-11-09 10:37:06 -0300

    Refactor routes to use controller methods

--- Vendas de Carros/routes/compradores.js
+++ Vendas de Carros/routes/compradores.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const CompradorModel = require('../models/compradorModel');
-router.get('/', async (req,res)=>{ const list = await CompradorModel.all(); res.render('compradores/index', { compradores: list, title:'Compradores' }); });
-router.get('/new', (req,res)=> res.render('compradores/form', { comprador:{}, title:'Novo Comprador' }));
-router.post('/', async (req,res)=>{ await CompradorModel.create(req.body); res.redirect('/compradores'); });
-// Terminar rotas de compradores (edit, delete)
+const ctrl = require('../controllers/compradorController');
+
+router.get('/', ctrl.index);
+router.get('/new', ctrl.newForm);
+router.post('/', ctrl.create);
+router.get('/:id/edit', ctrl.editForm);
+router.post('/:id', ctrl.update);
+router.post('/:id/delete', ctrl.remove);
+
 module.exports = router;
