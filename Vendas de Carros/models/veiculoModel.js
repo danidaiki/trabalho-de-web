9a9e428413fb4c5b0534b2b6dd44b2e5a3d9371f
Miguel Ap. <miguelaparecidocosta84@gmail.com>
2025-12-08 23:08:28 -0300

    Vendas de Carros directory modified

--- Vendas de Carros/models/veiculoModel.js
+++
@@ -1,6 +0,0 @@
-// Em desenvolvimento: completar funções de create, update, remove
-const db = require('../config/db');
-module.exports = {
-  async allWithVendedor(){ const [r]=await db.query(`SELECT v.*, ven.nome AS vendedor_nome FROM veiculo v LEFT JOIN vendedor ven ON v.id_vendedor=ven.id_vendedor ORDER BY v.id_veiculo DESC`); return r; }
-  // comletar funções create, update, remove
-};
--- Vendas de Carros/sql/drop.sql
+++
@@ -1,1 +0,0 @@
-DROP DATABASE IF EXISTS Vendas de Carros;
